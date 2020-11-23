572990f28495fd98e5cb4017603f5264c42c5aca
negative1skunk@gmail.com <negative1skunk@gmail.com>
2020-11-23 11:47:06 -0600

    book 4 finished

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,6 +1,6 @@
 import React from "react"
-import { Animal } from "./animal/Animal"
-import "./animal/Animal.css"
+import { LocationList } from "./location/LocationList"
+import { LocationProvider } from "./location/LocationProvider"
 import "./Kennel.css"
 
 export const Kennel = () => (
@@ -8,16 +8,9 @@ export const Kennel = () => (
     <h2>Nashville Kennels</h2>
     <small>Loving care when you're not there.</small>
 
-    <address>
-      <div>Visit Us at the Nashville North Location</div>
-      <div>500 Puppy Way</div>
-    </address>
-
-    <h2>Animals</h2>
-    <article className="animals">
-      <Animal />
-      <Animal />
-      <Animal />
-    </article>
+    <h2>Locations</h2>
+    <LocationProvider>
+      <LocationList />
+    </LocationProvider>
   </>
 )
